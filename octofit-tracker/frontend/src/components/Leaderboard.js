92bc88d3515a76e1bb8de017496147f44b01f264
Maamdi <aamdimed@outlook.com>
2026-02-19 12:42:23 +0000

    app4

--- octofit-tracker/frontend/src/components/Leaderboard.js
+++ octofit-tracker/frontend/src/components/Leaderboard.js
@@ -1,4 +1,6 @@
 
+import React, { useEffect, useState } from 'react';
+
 const Leaderboard = () => {
   const [leaders, setLeaders] = useState([]);
   const endpoint = `https://${process.env.REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/leaderboard/`;
